c411d92e2b410464e23363b78c52f3482ceea664
Po Yu Chen <pyshif@gmail.com>
2022-02-25 12:02:03 +0800

    增加狀態代碼：進度保存

---
+++ utils/status.js
@@ -0,0 +1,23 @@
+//
+// 狀態對照表（奇數：都是錯誤訊息，偶數：都是成功訊息）
+const PREFIX_ERROR = 'Error Msg :>> ';
+const PREFIX_STATUS = 'Status Msg :>> ';
+
+// export const STATUS_MSG = {
+//     4001: PREFIX_ERROR + '帳號或密碼錯誤',
+//     4002: PREFIX_STATUS + '登入成功',
+//     4003: PREFIX_ERROR + '已達嘗試次數上限',
+//     4004: PREFIX_STATUS + '註冊成功',
+//     4005: PREFIX_ERROR + 'Email 格式錯誤',
+//     4006: PREFIX_STATUS + '重設密碼信已寄出',
+//     4007: PREFIX_ERROR + '密碼不符合規定',
+//     4009: PREFIX_ERROR + '無效帳號或密碼提示關鍵字',
+//     4011: PREFIX_ERROR + '重設密碼嘗試已達上限',
+
+//     2002: PREFIX_STATUS + '成功取得資料庫資料',
+//     2003: PREFIX_ERROR + '資料庫無該筆資料',
+// };
+
+// export const STATUS_CODE = {
+//     ACCOUNT_PASSWORD_ERROR: 4001,
+// }
