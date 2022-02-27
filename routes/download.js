0ad4593889f9dc6c39d4b060242175cbcdd17fa8
Po Yu Chen <pyshif@gmail.com>
2022-02-28 03:11:22 +0800

    下載功能進度保存

--- routes/download.js
+++ routes/download.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 
 const compress = require('../middlewares/compress');
 
@@ -18,4 +19,60 @@ router.get('/', compress, function (req, res) {
     });
 });
 
+// 接收下載請求（會員）
+router.post('/', compress, function (req, res) {
+    console.log('req.body :>> ', req.body);
+    const p = path.join(__dirname, '../download', zipPahth(req.body.pid));
+    console.log('p :>> ', p);
+
+    // 存在檔案直接返回
+    if (fs.existsSync(p)) {
+        const fname = 'user-dng.zip';
+        res.setHeader('Content-Disposition', `filename=${fname}`);
+        fs.createReadStream(p).pipe(res);
+    }
+
+    const zipStream = new compressing.zip.Stream();
+
+    req.body.pid.forEach(function (e) {
+        zipStream.addEntry(path.join('../dng', e));
+    });
+
+    zipStream.pipe(fs.createWriteStream(p)).on('finish', function () {
+        res.setHeader('Content-Disposition', `filename=${fname}`);
+        fs.createReadStream(p).pipe(res);
+    });
+});
+
+// 接收下載請求（非會員）
+
 module.exports = router;
+
+// 內部函數
+function zipPath(products, ext = 'zip') {
+    products.sort((a, b) => a - b);
+    const cat = products.join('-') + '.' + ext;
+    return cat;
+}
+
+// 接收請求 (會員)
+//  request
+//      pid: []
+//      user: {id, name, }
+//      session: user
+
+// 接收請求（非會員）
+//  request
+//      link: ""
+
+// 驗證使用者身份
+
+// 生成壓縮檔名稱
+
+// 尋找 download 是否已存在壓縮檔，已存在則返回
+
+// 壓縮檔不存在則到 dng 資料夾將對應商品加入壓縮流
+
+// 將檔案壓縮後存放到 download 資料夾
+
+// 返回檔案下載流
