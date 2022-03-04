51c4c75b6b015b369ae43df86957397e139af106
jim60702 <95072583+jim60702@users.noreply.github.com>
2022-03-04 17:57:30 +0800

    蒐藏分支首次提交

---
+++ routes/favorites.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const connection = require('../utils/db');
+
+// const wishlistController = require('../controllers/wishlist');
+
+//取得全部資料
+router.get('/', async (req, res, next) => {
+    console.log('123');
+    const { userid } = req.query;
+
+    if (!userid) {
+        res.json({ error: '請輸入正確的 userid' });
+    }
+    console.log('userid :>> ', userid);
+    let sql = `SELECT
+                    id,
+                    created_at,
+                    product_id,
+                    user_id
+               FROM favorites
+               WHERE user_id = ?`;
+
+    try {
+        let [data, datafileds] = await connection.execute(sql, [userid]);
+        console.log('data :>> ', data);
+        // console.log('datafileds :>> ', datafileds);
+        res.json(data);
+    } catch (err) {
+        console.log('err :>> ', err);
+    }
+});
+
+// router.get('/test', wishlistController.getProducts);
+
+module.exports = router;
