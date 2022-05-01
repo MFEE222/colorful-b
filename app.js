d4f00c4e2eecab62ff7ef97aabf524fc0ed335cf
Po Yu Chen <pyshif@gmail.com>
2022-05-02 03:57:37 +0800

    增加 AWS ELB 測試路由 /health

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSession);
 
 // 路由中間件
+app.use('/health', function (req, res, next) {
+    res.status(200).send();
+});
 
 app.use('/api/auth', authRouter);
 app.use('/api/cart', cartRouter);
